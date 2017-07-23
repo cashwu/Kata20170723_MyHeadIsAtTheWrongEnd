472e77ce2ec61e369a45821f6ebb3c4af189dc84
cash <cash@cashwu.com>
2017-07-23 17:49:56 +0800

    first test

---
+++ Kata20170723_MyHeadIsAtTheWrongEnd/FixTheMeerkatTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170723_MyHeadIsAtTheWrongEnd
+{
+    [TestClass]
+    public class FixTheMeerkatTest
+    {
+        [TestMethod]
+        public void Input_tail_body_Should_Return_body_tail()
+        {
+            AssertFixTheMeerkatShouldBe(new[] {"tail", "body"}, new[] {"body", "tail"});
+        }
+
+        private static void AssertFixTheMeerkatShouldBe(string[] source, string[] expected)
+        {
+            var kata = new Kata();
+            var acutal = kata.FixTheMeerkat(source);
+            CollectionAssert.AreEqual(expected, acutal);
+        }
+    }
+
+    public class Kata
+    {
+        public string[] FixTheMeerkat(string[] arr)
+        {
+            return new[] {"body", "tail"};
+        }
+    }
+}
