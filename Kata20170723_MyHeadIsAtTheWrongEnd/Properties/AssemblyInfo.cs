7e21f9aec870ee0e2dfd19ef1f61a95e2529baed
cash <cash@cashwu.com>
2017-07-23 17:23:30 +0800

    Add project files.

---
+++ Kata20170723_MyHeadIsAtTheWrongEnd/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kata20170723_MyHeadIsAtTheWrongEnd")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kata20170723_MyHeadIsAtTheWrongEnd")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70efa682-c28b-4b2d-8ca3-547c17702e9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
